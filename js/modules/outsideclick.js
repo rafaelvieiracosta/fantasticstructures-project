ab467acdf26dcf9a19907f8ceebe4740863e3682
Rafael <rvc.vieira@outlook.com>
2022-05-13 11:16:28 -0300

    feat: menu mobile

--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -1,24 +1,24 @@
-export default function initDropdownMenu() {
-  function outsideClick(element, events, callback) {
-    const html = document.documentElement;
-    const outside = "data-outside";
+export default function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = "data-outside";
 
-    if (!element.hasAttribute(outside)) {
-      events.forEach((userEvent) => {
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      setTimeout(() => {
         html.addEventListener(userEvent, handleOutsideClick);
       });
-      element.setAttribute(outside, "");
-    }
+    });
+    element.setAttribute(outside, "");
+  }
 
-    function handleOutsideClick(event) {
-      if (!element.contains(event.target)) {
-        element.removeAttribute(outside, "");
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside, "");
 
-        events.forEach((userEvent) => {
-          html.removeEventListener(userEvent, handleOutsideClick);
-        });
-        callback();
-      }
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      callback();
     }
   }
 }
